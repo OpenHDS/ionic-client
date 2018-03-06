92e17747873edfe8a1a93312a0ac56b9dda6ba79
littlefieldnick <nickolas.littlefield@maine.edu>
2018-03-06 15:34:17 -0500

    Connected the Create location page to the errors page. Clicking on fix error, opens a filled out modal with the create location form with fields that need editing. Fixes #3

--- src/pages/error-display/error-display.ts
+++ src/pages/error-display/error-display.ts
@@ -2,9 +2,8 @@ import { Component } from '@angular/core';
 import {IonicPage, ModalController, NavController, NavParams} from 'ionic-angular';
 import {ErrorsProvider} from "../../providers/errors/errors";
 import {EntityErrorLabels} from "../../providers/errors/entity-error-labels";
-import {CreateLocationModalPage} from "../create-location-modal/create-location-modal";
-
 import {Errors} from "../../providers/errors/errors-db";
+import {CreateLocationPage} from "../create-location/create-location";
 import {LocationsProvider} from "../../providers/locations/locations-provider";
 
 /**
@@ -37,7 +36,7 @@ export class ErrorDisplayPage {
 
   //Fix errors that occur with creation or saving of a location.
   fixLocationErrors(locError: Errors){
-    const createPage = this.modalCtrl.create(CreateLocationModalPage, {fixError: true, location: locError.entity});
+    const createPage = this.modalCtrl.create(CreateLocationPage, {fixError: true, location: locError.entity});
     createPage.present();
 
     createPage.onDidDismiss(data => {
