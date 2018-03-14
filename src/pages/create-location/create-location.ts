fdac9715f91aafa7bf3eea05fe479acace243a64
littlefieldnick <nickolas.littlefield@maine.edu>
2018-03-14 17:50:44 -0400

    Refactoring of SystemConfigProvider. Provider loads in a json properties file. All properties are then stored in a configuration object, and accessed by the provider. (#9)

--- src/pages/create-location/create-location.ts
+++ src/pages/create-location/create-location.ts
@@ -4,8 +4,8 @@ import {Geolocation} from "@ionic-native/geolocation";
 import {Location} from "../../providers/locations/locations-db";
 import {NetworkConfigProvider} from "../../providers/network-config/network-config";
 import {LocationsProvider} from "../../providers/locations/locations-provider";
-import { FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
-import { SystemConf} from "../../providers/system-config/system-config";
+import { FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {SystemConfigProvider} from "../../providers/system-config/system-config";
 
 /**
  * Generated class for the CreateLocationPage page.
@@ -25,7 +25,6 @@ export class CreateLocationPage {
   geoloc: boolean = false;
   errorFix: boolean;
   locationForm: FormGroup;
-  sysConfig = SystemConf.getInstance();
 
   //Default for a new location being created. Values will be set if a location is being fixed (due to errors that may have occurred).
   loc: Location = {
@@ -45,8 +44,10 @@ export class CreateLocationPage {
     processed: 0
   };
 
-  constructor(public ev: Events, public navCtrl: NavController, public navParams: NavParams, public loadingCtrl: LoadingController, public formBuilder: FormBuilder, public locProvider: LocationsProvider,
-              public viewCtrl: ViewController, private geo: Geolocation, public netConfig: NetworkConfigProvider) {
+  constructor(public ev: Events, public navCtrl: NavController, public navParams: NavParams, public loadingCtrl: LoadingController,
+              public formBuilder: FormBuilder, public locProvider: LocationsProvider,
+              public viewCtrl: ViewController, private geo: Geolocation, public netConfig: NetworkConfigProvider,
+              public sysConfig: SystemConfigProvider) {
 
 
    this.locationForm = formBuilder.group({
@@ -82,7 +83,7 @@ export class CreateLocationPage {
       this.loc.altitude = resp.coords.altitude;
       this.loc.accuracy = resp.coords.altitudeAccuracy;
     }).catch(err => console.log(err)).then(() => {
-      this.geoloc = true
+      this.geoloc = true;
       loading.dismiss();
     });
   }
