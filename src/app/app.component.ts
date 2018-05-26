2cba3256d2041f41f6d8e8736c02b66dab3c78be
littlefieldnick <nickolas.littlefield@maine.edu>
2018-05-25 23:48:15 -0400

    Fixed error associated with the login page. When disabling back button, canSwipeBack should be passed as a parameter instead of trying to set it using a function call setCanSwipeBack. (Fixes #31).

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import {UserProvider} from "../providers/user-provider/user-provider";
 import {SupervisorModePage} from "../pages/supervisor-mode/supervisor-mode";
 import {SynchronizeDbPage} from "../pages/synchronize-db/synchronize-db";
 import {ApproveEntriesPage} from "../pages/approve-entries/approve-entries";
+import {SystemConfigPage} from "../pages/system-config/system-config";
 
 export interface PageInterface {
   title: string;
@@ -34,8 +35,8 @@ export class OpenHDSApp {
     { title: 'Dashboard', name: 'AdminDashboardPage', component: SupervisorModePage},
     { title: 'Baseline Census', name: 'BaselineCensusPage', component: BaselineCensusPage},
     { title: 'Synchronization', name: 'SynchronizeDbPage', component: SynchronizeDbPage},
-    { title: 'Data Entry Approval', name: 'ApproveEntriesPage', component: ApproveEntriesPage}
-
+    { title: 'Data Entry Approval', name: 'ApproveEntriesPage', component: ApproveEntriesPage},
+    { title: 'System Configurations', name: 'SystemConfigPage', component: SystemConfigPage},
   ];
 
   rootPage: any;
@@ -51,19 +52,12 @@ export class OpenHDSApp {
     this.platformReady();
   }
 
-  isActive(page){
-    if (this.nav.getActive() && this.nav.getActive().name === page.name) {
-      return 'primary';
-    }
-    return;
-  }
-
   openPage(page){
     this.appCtrl.getRootNav().push(page.component);
   }
 
   logoutUser() {
-    this.appCtrl.getRootNav().setRoot(LoginPage).showBackButton(false);
+    this.appCtrl.getRootNav().setRoot(LoginPage, {canSwipeBack: false});
     this.userData.setUserLogout();
   }
 
