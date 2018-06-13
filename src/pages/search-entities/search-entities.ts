e485e5ba12127306bf2e73d71042d2423ec421e4
littlefieldnick <nickolas.littlefield@maine.edu>
2018-06-12 20:14:48 -0400

    Searching for entities: Refactoring to eliminate redundant code. (#34)

--- src/pages/search-entities/search-entities.ts
+++ src/pages/search-entities/search-entities.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {IonicPage, NavController, NavParams} from 'ionic-angular';
 import {SocialGroupProvider} from "../../providers/social-group/social-group";
 import {LocationsProvider} from "../../providers/locations/locations-provider";
 import {IndividualProvider} from "../../providers/individual/individual";
@@ -19,7 +19,7 @@ import {Location} from "../../interfaces/locations";
   selector: 'page-search-entities',
   templateUrl: 'search-entities.html',
 })
-export class SearchEntitiesPage implements OnInit{
+export class SearchEntitiesPage implements OnInit {
   filteredLocations: Location[];
   filteredSocialGroups: SocialGroup[];
   filteredIndividuals: Individual[];
@@ -29,75 +29,60 @@ export class SearchEntitiesPage implements OnInit{
               public locProvider: LocationsProvider, public indProvider: IndividualProvider) {
   }
 
-  ngOnInit(){
-    this.loadList()
+  async ngOnInit() {
+    await this.loadList()
   }
 
-  loadList(){
-    switch(this.entityType){
+  async loadList() {
+    switch (this.entityType) {
       case 'locations':
-        this.locProvider.getAllLocations().then(x => this.filteredLocations = x);
+        await this.locProvider.getAllLocations().then(x => this.filteredLocations = x);
       case 'socialgroups':
-        this.sgProvider.getAllSocialGroups().then(x => this.filteredSocialGroups = x);
+        await this.sgProvider.getAllSocialGroups().then(x => this.filteredSocialGroups = x);
       case 'individuals':
-        this.indProvider.getAllIndividuals().then(x => this.filteredIndividuals = x);
+        await this.indProvider.getAllIndividuals().then(x => this.filteredIndividuals = x);
     }
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad SearchEntitiesPage');
+  async search(query) {
+    var searchFor = query.target.value;
+    await this.loadList();
+    if (searchFor && searchFor.trim() != '') {
+      switch (this.entityType) {
+        case 'locations':
+          await this.searchLocations(searchFor);
+        case 'socialgroups':
+          await this.searchSocialGroups(searchFor);
+        case 'individuals':
+          await this.searchForIndividuals(searchFor);
+      }
+    }
+
   }
 
-  async getResults(event){
-    switch(this.entityType){
-      case 'locations':
-        await this.searchLocations(event);
-      case 'socialgroups':
-        await this.searchSocialGroups(event);
-      case 'individuals':
-        await this.searchForIndividuals(event);
-    }
+  async searchLocations(query) {
+    this.filteredLocations = this.filteredLocations.filter((item) => {
+      return (item.extId.toLowerCase().indexOf(query.toLowerCase()) > -1) ||
+        (item.locationName.toLowerCase().indexOf(query.toLowerCase()) > -1)
+    })
 
-    }
+  }
 
-    async searchLocations(query){
-      this.filteredLocations = await this.locProvider.getAllLocations();
+  async searchSocialGroups(query) {
+    this.filteredSocialGroups = this.filteredSocialGroups.filter((item) => {
+      return (item.extId.toLowerCase().indexOf(query.toLowerCase()) > -1) ||
+        (item.groupName.toLowerCase().indexOf(query.toLowerCase()) > -1) ||
+        (item.groupHead.extId.toLowerCase().indexOf(query.toLowerCase()) > -1);
+    })
 
-      var searchFor = query.target.value;
+  }
 
-      if(searchFor && searchFor.trim() != ''){
-        this.filteredLocations = this.filteredLocations.filter((item) => {
-          return (item.extId.toLowerCase().indexOf(searchFor.toLowerCase()) > -1) ||
-            (item.locationName.toLowerCase().indexOf(searchFor.toLowerCase()) > -1)
-        })
-      }
-    }
-    async searchSocialGroups(query){
-      this.filteredSocialGroups = await this.sgProvider.getAllSocialGroups();
-
-      var searchFor = query.target.value;
-
-      if(searchFor && searchFor.trim() != ''){
-        this.filteredSocialGroups = this.filteredSocialGroups.filter((item) => {
-          return (item.extId.toLowerCase().indexOf(searchFor.toLowerCase()) > -1) ||
-            (item.groupName.toLowerCase().indexOf(searchFor.toLowerCase()) > -1) ||
-            (item.groupHead.extId.toLowerCase().indexOf(searchFor.toLowerCase()) > -1);
-        })
-      }
-    }
-
-    async searchForIndividuals(query){
-      this.filteredIndividuals = await this.indProvider.getAllIndividuals();
-
-      var searchFor = query.target.value;
-
-      if(searchFor && searchFor.trim() != ''){
-        this.filteredIndividuals = this.filteredIndividuals.filter((item) => {
-          return (item.extId.toLowerCase().indexOf(searchFor.toLowerCase()) > -1) ||
-            (item.firstName.toLowerCase().indexOf(searchFor.toLowerCase()) > -1) ||
-            (item.lastName.toLowerCase().indexOf(searchFor.toLowerCase()) > -1);
-        })
-      }
-    }
+  async searchForIndividuals(query) {
+    this.filteredIndividuals = this.filteredIndividuals.filter((item) => {
+      return (item.extId.toLowerCase().indexOf(query.toLowerCase()) > -1) ||
+        (item.firstName.toLowerCase().indexOf(query.toLowerCase()) > -1) ||
+        (item.lastName.toLowerCase().indexOf(query.toLowerCase()) > -1);
+    })
+  }
 
 }
