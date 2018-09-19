9e36ded0829dcb80b74885acc117e5f41755167b
littlefieldnick <nickolas.littlefield@maine.edu>
2018-09-18 23:12:11 -0400

    Ionic 4 Upgrade: Individual Listings -- Separated out listings of the individuals to select from and the creation of individuals. (#55)

--- src/app/components/individual-list/individual-list.component.ts
+++ src/app/components/individual-list/individual-list.component.ts
@@ -1,15 +1,80 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {RefreshObservable} from "../../services/RefreshObservable";
+import {Individual} from "../../models/individual";
+import {SocialGroup} from "../../models/social-group";
+import {Events} from "@ionic/angular";
+import {IndividualService} from "../../services/IndividualService/individual.service";
 
 @Component({
-  selector: 'app-individual-list',
+  selector: 'individual-list',
   templateUrl: './individual-list.component.html',
   styleUrls: ['./individual-list.component.scss']
 })
 export class IndividualListComponent implements OnInit {
+  itemsPerPage = 7;
+  selectedPage = 1;
 
-  constructor() { }
+  indObserver: RefreshObservable = new RefreshObservable();
+  @Input() sg: SocialGroup;
+  @Input() loc: Location;
+  @Input() collectedBy: string;
+  @Output() selectedIndividual = new EventEmitter<Individual>();
+  individuals: Individual[];
 
-  ngOnInit() {
+  constructor(public ev: Events, public indProvider: IndividualService) {
+    this.ev.subscribe("submitIndividual", async (ind) => {
+      await this.getAllIndividuals().catch(err => console.log(err))
+        .then(() =>
+        {
+          this.individuals = this.filterBySGExtId();
+          this.selectIndividual(ind["ind"]);
+        });
+    });
+
+    this.ev.subscribe('syncDb', () => {
+      this.indProvider.loadInitData().then(async () => await this.getAllIndividuals()).catch(err => console.log(err));
+    });
+
+    this.indObserver.subscribe(async (ind) => {
+      this.individuals = ind;
+    });
+  }
+
+  async ngOnInit() {
+    await this.getAllIndividuals().catch(err => console.log(err));
+  }
+
+  async getAllIndividuals(){
+    let ind = await this.indProvider.getAllIndividuals();
+    this.indObserver.publishChange(ind);
+  }
+
+  filterBySGExtId(){
+    if(this.individuals == null)
+      return [];
+    return this.individuals.filter(ind => ind.extId.startsWith(this.sg.extId));
+  }
+
+  selectIndividual(ind){
+    this.selectedIndividual.emit(ind);
+  }
+
+  changePage(page) {
+    this.selectedPage = page;
+  }
+
+  get pageCount(): number {
+    if(this.individuals != undefined)
+      return Math.ceil(this.individuals.length/this.itemsPerPage);
+
+    return 0;
+  }
+
+  get individualDetails(): Individual[] {
+    let pageIndex = (this.selectedPage - 1) * this.itemsPerPage;
+    if(this.individuals != undefined)
+      return this.individuals.slice(pageIndex, pageIndex + this.itemsPerPage);
+    return [];
   }
 
 }
