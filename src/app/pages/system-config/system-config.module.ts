be8b79aabc17148de6b8ad2d7522cabad107a2fd
littlefieldnick <nickolas.littlefield@maine.edu>
2018-11-09 16:51:08 -0500

    Modified system configurations module to include page declaration that was not declared with ionic update. (#55)

--- src/app/pages/system-config/system-config.module.ts
+++ src/app/pages/system-config/system-config.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {SystemConfigPage} from "./system-config.page";
+import {ComponentsModule} from "../../components/components";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {IonicModule} from "@ionic/angular";
+import {CommonModule} from "@angular/common";
 
 const routes: Routes = [
   {
@@ -10,7 +14,14 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    ComponentsModule,
+    CommonModule,
+    FormsModule, ReactiveFormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+
+  declarations: [SystemConfigPage]
 })
 export class SystemConfigPageModule { }
--- src/app/pages/system-config/system-config.page.ts
+++ src/app/pages/system-config/system-config.page.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {Router} from "@angular/router";
 import {SystemConfigService} from "../../services/SystemService/system-config.service";
 
 @Component({
